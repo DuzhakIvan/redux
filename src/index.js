8d6bf3e2cb1ba39cd50f525b2b0452ec55b5ce1d
IvanDuzhak <ivanduzhak@gmail.com>
2023-06-08 15:50:28 +0300

    Imitation Redux logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-console.log('Hello Redux!');
+// CREATE COUNTER
+const initialState = 0;
+
+// create function reducer by logical block scheme of Redux
+const reducer = (state = 0, action) => { // reducer get state (what state now) and action (what action will be provide on state), state = 0 - initial parametr
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+}
+
+// Initialization global state
+let state = reducer(initialState, {type: 'INC'}); // first time we use initialState, In action we must write object {type} 
+state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
+state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
+state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
+console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
