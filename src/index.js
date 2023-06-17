0683376d3b03ce382834d9c149ad580f1ef24a60
IvanDuzhak <ivanduzhak@gmail.com>
2023-06-18 00:05:09 +0300

    Simple counter with Redux

--- src/index.js
+++ src/index.js
@@ -1,32 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 
 // CREATE COUNTER
-const initialState = 0;
+const initialState = {value: 0};
 
-// create function reducer by logical block scheme of Redux
-const reducer = (state = 0, action) => { // reducer get state (what state now) and action (what action will be provide on state), state = 0 - initial parametr
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        default:
-            return state;
-    }
+const reducer = (state = initialState, action) => { 
+switch (action.type) {
+    case 'INC':
+        return {
+            ...state,
+            value: state.value + 1
+        }; 
+    case 'DEC':
+        return  {
+            ...state,
+            value: state.value - 1
+        };
+    case 'RND':
+        return  {
+            ...state,
+            value: state.value * action.payload
+        };
+    default:
+        return state;
+}
 }
 
-// Initialization global state
-let state = reducer(initialState, {type: 'INC'}); // first time we use initialState, In action we must write object {type} 
-state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
-state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
-state = reducer(state, {type: 'INC'}); // state, in action we must write object {type} 
-console.log(state);
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState().value; 
+}
+
+store.subscribe (update);
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', payload: value}); 
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+})
+
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+    store.dispatch(rnd(Math.floor(Math.random()* 10)));
+})
+store.dispatch({type: "INC"}); 
+store.dispatch({type: "INC"}); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <>
+    <React.StrictMode>
+        <>
 
-    </>
-  </React.StrictMode>
+        </>
+    </React.StrictMode>
 );
+
 
